8251bbee6f3bd9beebcb7a9edf3eb46acb2af0da
Ubuntu <ubuntu@sonaaz.com>
2025-09-13 08:06:02 +0000

    add development server

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,18 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+
+// export default defineConfig({
+//   base: "/data-agent/",
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "src"),
+//     },
+//   },
+// });
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
@@ -9,5 +24,8 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+  },
+  server: {
+    allowedHosts: ["ai.samodrei.com"], // 👈 add your domain here
   },
 });
